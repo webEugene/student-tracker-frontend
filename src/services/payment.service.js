914e79be89e3e21feeca5584d5011f98eb312552
Eugene Bilan <eugen.bilan@gmail.com>
2023-12-28 00:06:18 +0200

    tariff-settings

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,16 +1,27 @@
 import { axiosHandler } from '@/axios.config';
 // import getCompanyId from '../helpers/getCompanyId';
 
-export { PaymentService }
+export { PaymentService };
 class PaymentService {
-    /**
-     * Create payment and get Fondy checkout URL
-     *
-     * @returns {Promise<AxiosResponse<any>>}
-     */
-    static createPaymentFondy(params) {
-        return axiosHandler.post(`${process.env.VUE_APP_API_ENDPOINT}/v1/payments/fondy`,{
-            ...params,
-        });
-    }
+  /**
+   * Create payment and get Fondy checkout URL
+   *
+   * @returns {Promise<AxiosResponse<any>>}
+   */
+  static createPaymentFondy(params) {
+    return axiosHandler.post(`${process.env.VUE_APP_API_ENDPOINT}/v1/payments/fondy`, {
+      ...params,
+    });
+  }
+
+  /**
+   * Create payment and get Fondy checkout URL
+   *
+   * @returns {Promise<AxiosResponse<any>>}
+   */
+  static createPaymentFormLiqPay(params) {
+    return axiosHandler.post(`${process.env.VUE_APP_API_ENDPOINT}/v1/payments/liqpay`, {
+      ...params,
+    });
+  }
 }
---
+++ src/views/Payment/PaymentForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid>
+    <div v-html="form"></div>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'PaymentForm',
+  data: () => ({
+    form: null,
+  }),
+  mounted() {
+    this.form = '';
+  },
+};
+</script>
